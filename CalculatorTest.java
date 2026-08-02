443d7cd25878363d8aeca4df0f2cda3bd7d7182d
shaikhfaiza78009 <shaikhfaiza78009@gmail.com>
2026-08-02 17:00:31 +0530

    Add files via upload

---
+++ CalculatorTest.java
@@ -0,0 +1,8 @@
+public class CalculatorTest {
+    
+    public static void main(String[] args) {
+        Calculator c1 = new Calculator();
+        c1.add(5,10);
+        c1.add(4.5 ,4.5);
+    }
+}
